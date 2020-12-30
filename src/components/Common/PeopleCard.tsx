d35f40b88035a73a28fa93b179a45c637a9e9d05
Aramex86 <slavbez@mail.ru>
2020-12-30 14:18:43 +0200

    peple card fix names and noImage

--- src/components/Common/PeopleCard.tsx
+++ src/components/Common/PeopleCard.tsx
@@ -1,5 +1,6 @@
 import React, {FC} from 'react';
 import {AllMediaType} from '../../Types/Types';
+import NoImage from '../../assets/noImage.png';
 
 type PropsType = {
   name: string;
@@ -8,23 +9,23 @@ type PropsType = {
 };
 
 const PeopleCard: FC<PropsType> = ({name, photo, tapes}) => {
-  const movies = tapes.map((item) => (
-    <span>{`${item.title? item.title: item.name},`} </span>
-  ));
-
-  console.log(movies);
+  const movies = tapes.map(
+    (item) => `${item.title ? item.title : item.name} , `
+  );
 
   return (
     <div className="card__wrap">
       <div className="card__wrap_img">
-        <img src={`https://image.tmdb.org/t/p/w500${photo}`} alt="" />
+        {photo === null ? (
+          <img src={NoImage} alt="" />
+        ) : (
+          <img src={`https://image.tmdb.org/t/p/w500${photo}`} alt="" />
+        )}
       </div>
       <div className="card__wrap_desc">
         <span className="card__wrap_name">{name}</span>
         <br />
-        <div className="card__wrap_tapes" >
-          {movies}
-        </div>
+        <div className="card__wrap_tapes">{movies}</div>
       </div>
     </div>
   );
